9a9fd9ddbf57960c5adbec5424d89187628a0b8c
Vance Newberry <vancenewberry@gmail.com>
2020-07-08 18:04:18 -0500

    Updates

---
+++ precache-manifest.b64f2342cbd3460a65ec740e0bd52592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f26bff7ea7444befba22807a89159a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "437c6efb72548bc07e29",
+    "url": "/clicky-game/static/css/main.4bc39285.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437c6efb72548bc07e29",
+    "url": "/clicky-game/static/js/main.839fefb5.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
